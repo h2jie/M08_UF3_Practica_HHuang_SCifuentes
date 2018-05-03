a16cb55401c1e8cd18d034c3aa7f985cd360917e
h2jie <h2jie09@gmail.com>
2018-05-03 17:47:53 +0200

    views created

---
+++ M08_UF3_Practica_HHuang_SCifuentes/LoadUIViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoadUIViewController.swift
+//  M08_UF3_Practica_HHuang_SCifuentes
+//
+//  Created by Hangjie Huang on 2018/5/3.
+//  Copyright © 2018年 Hangjie Huang. All rights reserved.
+//
+
+import UIKit
+
+class LoadUIViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
