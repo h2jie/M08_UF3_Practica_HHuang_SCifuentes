bdaad6acd7d4c3f19e186fddd4ca9aed7e2a4f78
h2jie <h2jie09@gmail.com>
2018-05-03 16:39:30 +0200

    Initial Commit

---
+++ M08_UF3_Practica_HHuang_SCifuentes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  M08_UF3_Practica_HHuang_SCifuentes
+//
+//  Created by Hangjie Huang on 2018/5/3.
+//  Copyright © 2018年 Hangjie Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
